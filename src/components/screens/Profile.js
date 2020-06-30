0b0b50d08d4c4d91c7d210bda7be43ee2728a73c
unknown <andresch.denes@gmail.com>
2020-06-30 17:28:55 +0200

    profile

--- src/components/screens/Profile.js
+++ src/components/screens/Profile.js
@@ -1,7 +1,61 @@
 import React, { Component } from "react";
 
 const Profile = () => {
-  return <h1>Profile</h1>;
+  return (
+    <div style={{ maxWidth: "600px", margin: "0px auto" }}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          margin: "18px 0px",
+          borderBottom: "1px solid grey",
+        }}
+      >
+        <div>
+          <img
+            style={{ width: 160, height: 160, borderRadius: 80 }}
+            src="https://images.unsplash.com/photo-1587173399514-ebfc0f4fa396?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=784&q=80"
+          />
+        </div>
+        <div>
+          <h4>andras</h4>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              width: "108%",
+            }}
+          >
+            <h6>40 posts</h6>
+            <h6>40 followers</h6>
+            <h6>40 following</h6>
+          </div>
+        </div>
+      </div>
+      <div className="gallery">
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1587173399514-ebfc0f4fa396?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=784&q=80"
+        />
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1587173399514-ebfc0f4fa396?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=784&q=80"
+        />
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1587173399514-ebfc0f4fa396?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=784&q=80"
+        />
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1587173399514-ebfc0f4fa396?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=784&q=80"
+        />
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1587173399514-ebfc0f4fa396?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=784&q=80"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
